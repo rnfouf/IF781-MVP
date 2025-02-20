21cd49223c35a4657320d8c052ae2c3ec63a285f
rnfouf <renato.ferraz@ufpe.br>
2025-02-19 21:23:36 -0300

    Fixed backend port to 8080

--- backend/src/server.js
+++ backend/src/server.js
@@ -50,7 +50,7 @@ const startServer = async () => {
     app.use("/api/companies", companyRoutes); // Register company routes
     console.log("Company routes registered at /api/companies");
 
-    const PORT = process.env.PORT || 5000;
+    const PORT = 8080;
     app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
   } catch (error) {
     console.error("❌ Failed to start server:", error);
